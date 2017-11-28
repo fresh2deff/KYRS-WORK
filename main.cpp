761ceb872c3e36a62dece41c53e94c4e2e3b7979
vlaf27449 <33995288+vlaf27449@users.noreply.github.com>
2017-11-28 17:34:34 +0700

    Функция Площади

--- main.cpp
+++ main.cpp
@@ -2,13 +2,20 @@
 #include <iostream>
 #include <math.h>
  
-int main()//=========================================================
+
+    
+STreug(int a, int b)//=========================================================
 {
-    int a = 20;
-    int b = 8;
+   
     double Y = 45;// ???????
     double S = a*b*sin(Y*M_PI/180)/2;
     std::cout << S << std::endl;    
  
     return 0;
 }
+
+ main(){
+ 	
+ 	STreug(20, 20);
+ 	
+ }
