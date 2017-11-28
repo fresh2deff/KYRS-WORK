2b169b37820ed76c19bf9ee1d1e5cb1a47f215a2
vlaf27449 <33995288+vlaf27449@users.noreply.github.com>
2017-11-28 17:52:08 +0700

    Условия

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@
  
 
     
-STreug(int a, int b)//=========================================================
+STreug(int a, int b, int y)//=========================================================
 {
+   if (a<0||b<0||y<0||y>180){
+   	std::cout << "Eror" << std::endl;
+   }
+
    
-    double Y = 45;// ???????
-    double S = a*b*sin(Y*M_PI/180)/2;
+    
+    double S = a*b*sin(y*M_PI/180)/2;
     std::cout << S << std::endl;    
  
     return 0;
@@ -16,6 +20,6 @@ STreug(int a, int b)//=========================================================
 
  main(){
  	
- 	STreug(20, 20);
+ 	STreug(20, 20, 90);
  	
  }
